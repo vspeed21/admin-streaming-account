76dd4fe1d2071df9092b90a70bb8056a60a0d3fe
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-14 16:34:53 -0600

    AlertaInputs: change font size on tables & pc

--- src/components/AlertaInputs.jsx
+++ src/components/AlertaInputs.jsx
@@ -1,6 +1,6 @@
 
 function AlertaInputs({msg}) {
-  return <p className="text-center text-sm text-red-700 font-bold">{msg}</p>
+  return <p className="text-center text-sm lg:text-base text-red-700 font-bold">{msg}</p>
 }
 
 export default AlertaInputs;
