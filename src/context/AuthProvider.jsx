171b1844f420bf882d4c040226c8827fedc06a97
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-16 11:01:39 -0600

    context: checkAuth function created for verify token & fetch to api

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,14 +1,47 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import adminClient from '../config/axios';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
   const [auth, setAuth] = useState({});
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    checkAuth();
+    async function checkAuth() {
+      const token = localStorage.getItem('asa-token');
+      if(!token){
+        setCargando(false);
+        return;
+      }
+      const config = {
+        headers:{
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        }
+      }
+
+      try {
+        const { data } = await adminClient('/admin/perfil', config);
+        setAuth(data);
+      } catch (error) {
+        console.log(error);
+        setAuth({})
+      }
+
+      setCargando(false);
+    }
+
+  }, [])
+  
 
   return(
     <AuthContext.Provider
       value={{
         auth,
+        cargando,
+        setAuth,
       }}
     >
       {children}
