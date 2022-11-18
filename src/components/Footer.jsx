be5b6e465cfcf8335ed04066c1d517c902d1d8c6
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-17 18:10:48 -0600

    Footer component done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+  return (
+    <footer className='text-center font-bold text-xl'>
+      <span className="text-red-600">ASA </span>
+      - Administrador de cuentas de Streaming
+    </footer>
+  )
+}
+
+export default Footer
