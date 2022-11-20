a48ae067c9dee7ea487af256a7c7f7326ae4b6de
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-19 20:56:51 -0600

    getAccounts & saveAccounst function created on account provider

--- src/context/AccountProvider.jsx
+++ src/context/AccountProvider.jsx
@@ -1,14 +1,51 @@
 import {createContext, useState, useEffect } from "react";
+import adminClient from "../config/axios";
 
 const AccountContext = createContext();
 
 export function AccountProvider({children}) {
   const [accounts, setAccounts] = useState([]);
+  console.log(accounts);
+
+  const token = localStorage.getItem('asa-token');
+  const config = {
+    headers:{
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    }
+  }
+
+  useEffect(() => {
+    getAccounts();
+
+    async function getAccounts() {
+      if(!token) return;
+      try {
+        const { data } = await adminClient('/account', config);
+        setAccounts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, []);
+
+  const saveAccounts = async account => {
+    if(!token) return;
+    try {
+      const { data } = await adminClient.post('/account', account, config);
+      const { updatedAt, createdAt, __v, ...accountsSave } = data;
+      setAccounts([accountsSave, ...accounts]);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return(
     <AccountContext.Provider
       value={{
-        accounts
+        accounts,
+        saveAccounts,
       }}
     >
       {children}
